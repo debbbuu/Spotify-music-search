205da9a8c49fcc78efded1903fea803c65c433d6
debbbuu <surajnjadhav27@gmail.com>
2022-04-04 13:28:33 +0530

    Files Uploaded, initial commit

---
+++ src/components/Tracks.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import _ from 'lodash';
+import music from '../images/music.jpeg';
+
+const Tracks = ({ tracks }) => {
+  return (
+    <React.Fragment>
+      {Object.keys(tracks).length > 0 && (
+        <div className="tracks">
+          {tracks.items.map((track, index) => {
+            return (
+              <React.Fragment key={index}>
+                <Card style={{ width: '18rem' }}>
+                  <a
+                    target="_blank"
+                    href={track.external_urls.spotify}
+                    rel="noopener noreferrer"
+                    className="card-image-link"
+                  >
+                    {!_.isEmpty(track.images) ? (
+                      <Card.Img
+                        variant="top"
+                        src={track.images[0].url}
+                        alt=""
+                      />
+                    ) : (
+                      <img src={music} alt="" />
+                    )}
+                  </a>
+                  <Card.Body>
+                    <Card.Title>{track.name}</Card.Title>
+                    <Card.Text>
+                      <small>
+                        {track.artists.map((artist) => artist.name).join(', ')}
+                      </small>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </React.Fragment>
+            );
+          })}
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Tracks;
